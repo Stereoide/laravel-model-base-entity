54affc58a37fefdf0e3e4a2da2b035fbe1305ace
Tobias Wanders <Stereoide@users.noreply.github.com>
2016-09-23 10:31:09 +0200

    Initial commit after blank package creation

---
+++ src/ModelBaseEntityServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Stereoide\ModelBaseEntity;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModelBaseEntityServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
